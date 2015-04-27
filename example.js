133f130d28f3c37d2992fb88c4f9d017fa7f8a4c
Dinukshan Kuruppu <dinuks@gmail.com>
2015-04-27 11:43:20 +0800

    Example Updates

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 var mockup = require('./index');
 
 mockup.create({
-  url: ['https://flinc.org', 'https://flinc.org/drivers'],
-  template: 'mobile_multiple', // available: 'flat_responsive', 'flat_responsive_2'
+  url: 'https://flinc.org',
+  template: 'flat_responsive', // available: 'flat_responsive', 'flat_responsive_2'
   output: 'responsive_mockup.png'
 });
