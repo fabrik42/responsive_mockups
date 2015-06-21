9176eb3741f54838a1d927c6769ff00c84e42b53
Christian Bäuerlein <fabrik42@gmail.com>
2015-06-21 17:39:51 +0200

    better example urls

--- example-mobile-multiple.js
+++ example-mobile-multiple.js
@@ -2,12 +2,12 @@ var mockup = require('./index');
 
 mockup.create({
   url: [
-    'https://flinc.org',
     'https://flinc.org/drivers',
+    'https://flinc.org',
     'https://flinc.org/passengers',
     'https://flinc.org/corporate',
-    'https://flinc.org/developers',
-    'https://support.flinc.org/hc/en-us/categories/200236098-FAQ'
+    'https://flinc.org/users/sign_up',
+    'https://flinc.org/users/sign_in'
   ],
   template: 'mobile_multiple', // available: 'flat_responsive', 'flat_responsive_2'
   output: 'responsive_mockup.png'
